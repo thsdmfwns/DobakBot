4de25f99fc07d54066ff0a14e9f3f1e9fa8f827a
son <son2044@gmail.com>
2021-12-18 00:15:14 +0900

    mp5추가

--- DobakBot/Model/Weapon.cs
+++ DobakBot/Model/Weapon.cs
@@ -26,6 +26,7 @@ namespace DobakBot.Model
             list.Add(new Weapon("Knife", 1000, "자루"));
             list.Add(new Weapon("Desert Eagle", 2200, "정"));
             list.Add(new Weapon("12GAUGE Shotgun", 2500, "정"));
+            list.Add(new Weapon("MP5", 3500, "정"));
             list.Add(new Weapon("9mm", 50, "탄창"));
             list.Add(new Weapon("7.62mm", 80, "탄창"));
             list.Add(new Weapon("5.56mm", 70, "탄창"));
