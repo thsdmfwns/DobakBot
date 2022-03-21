64812e4146e88f76bf6711446fb4a3b7e126e243
을준 손 <son@sonserver.xyz>
2022-03-22 00:00:15 +0900

    1

--- DobakBot/Controller/Module/AdminMoudule.cs
+++ DobakBot/Controller/Module/AdminMoudule.cs
@@ -9,10 +9,9 @@ using System.Threading.Tasks;
 
 namespace DobakBot.Controller
 {
-    [RequireRole("CASINO dealer")]
+    [RequireRole("dealer")]
     public class AdminMoudule : ModuleBase<SocketCommandContext>
     {
-        [RequireChannel("사용안함")]
         [Command("입장버튼")]
         public async Task ButtonSpawn()
         {
@@ -20,7 +19,8 @@ namespace DobakBot.Controller
             var builder = new EmbedBuilder();
             builder.Title = ":shinto_shrine: 카지노 입장";
             builder.Color = Color.Green;
-            builder.Description = "카지노에 입장하시기 전, \n 디스코드 채널의 별명을 IC상의 이름으로 설정해주세요.\n" +
+            builder.Description = 
+                "카지노에 입장하시기 전, \n 디스코드 채널의 별명을 IC상의 이름으로 설정해주세요.\n" +
                 "IC상에서의 코인 충전과 코인 환전을 위해 필요합니다.";
             await ReplyAsync("", embed: builder.Build(), component: button.Build());
         }
